07f316b31b09d1764bcf8434e567289845a5cdea
Wayne <weiqiang.zhang@mail.utoronto.ca>
2020-03-31 23:23:03 -0400

    added error message

--- exercise/src/store/actions/signupAction.js
+++ exercise/src/store/actions/signupAction.js
@@ -6,7 +6,9 @@ export const signup = (email, password) => {
     const auth = firebase.auth();
     return (dispatch) => {
         dispatch({type: actionTypes.SIGN_UP_LOADING,});
-        auth.createUserWithEmailAndPassword(email, password)
+        auth.createUserWithEmailAndPassword(email, password).then(res =>
+            {dispatch({type: actionTypes.SIGN_UP_SUCCESS})}
+        )
         .catch(e => 
           {dispatch({type: actionTypes.SIGN_UP_FAILED, errorMessage: e.message})}
         );
